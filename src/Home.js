e33af43d2986cce9a9e7ebb34b045a3d1ef99c6f
rengasayee <rsrinivasarengasayee@gmail.com>
2024-06-18 23:43:02 +0530

    Tested how buttons with onClick works, anonymous fns

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello, World!',e);
+    }
+    const handleClickAgain = (name,e) => {
+        console.log(`hello ${name}`,e);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handleClick}>Click Me</button>
+            <button onClick = {
+                (e) => {
+                    handleClickAgain('ani',e);
+                }
+            }>Click again</button>
         </div>
      );
 }
